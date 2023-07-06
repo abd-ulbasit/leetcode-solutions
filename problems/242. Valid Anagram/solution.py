64cc28c7c1975c0b01f29fadf30cdd0866f3347e
abd-ulbasit <basit01035@gmail.com>
2023-07-06 19:29:46 +0500

    Valid Anagram

---
+++ problems/242. Valid Anagram/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        s_hash,t_hash={},{}
+        for i in range(len(s)):
+            s_hash[s[i]]=1+s_hash.get(s[i],0)
+            t_hash[t[i]]=1+t_hash.get(t[i],0)
+        return s_hash==t_hash
+        # Time complexity : O(n) n is length of string s/t
+        # Space complexity : 0(n) n is no of characters in s/t 
